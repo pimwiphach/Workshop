43c6d1fc52178469b4bdd8c3efd74697a7a5cec1
Pimwipha Cheaptumrong <pimwiphach.office@gmail.com>
2025-05-17 15:08:40 +0700

    Local edit

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,7 +5,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //From B
+        //From BBB
     }
 
     // Update is called once per frame
